ba527e96eb68aa8201ceef52386c0dc5aa5581bf
ammartin8 <ammartin8249@gmail.com>
2020-05-26 17:36:21 -0400

    Updates

---
+++ precache-manifest.a578f2d984950e2e8c5a3bd4a841e868.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9c7aa6cd950d2e8484e189f0642b38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22f3a7c2d3f1653fa67",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f0d50809278bb47cd759",
+    "url": "/static/css/main.cdc64a0c.chunk.css"
+  },
+  {
+    "revision": "e22f3a7c2d3f1653fa67",
+    "url": "/static/js/2.4f1ebd56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4f1ebd56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d50809278bb47cd759",
+    "url": "/static/js/main.aa3f5980.chunk.js"
+  },
+  {
+    "revision": "8e0d6f1bd75f47696a88",
+    "url": "/static/js/runtime-main.f3f540e9.js"
+  },
+  {
+    "revision": "74834f1a6b1090ad2cef274280fea9b7",
+    "url": "/static/media/illustrated_profile.74834f1a.svg"
+  },
+  {
+    "revision": "31bf2fcbd056ca5e5f0c774f93753e40",
+    "url": "/static/media/jumbo_background_final.31bf2fcb.jpg"
+  },
+  {
+    "revision": "14a0d5c59883dfbf52bc2a68cde7e77f",
+    "url": "/static/media/musicLandingSite.14a0d5c5.png"
+  },
+  {
+    "revision": "6303fe275acd9fdbdb6a7ebe6c41d9b2",
+    "url": "/static/media/photoPortfolioSite.6303fe27.png"
+  },
+  {
+    "revision": "1fc691a8b11307359b2392d51b80f2c9",
+    "url": "/static/media/placeimg_864_576_tech.1fc691a8.jpg"
+  },
+  {
+    "revision": "099b561c35ce378f1c471bc71a92a356",
+    "url": "/static/media/techLandingSite.099b561c.png"
+  }
+]);
