ce6d3491f7d649c54a7668cbbd5a1c7b3b2a9367
ammartin8 <ammartin8249@gmail.com>
2020-05-26 17:24:12 -0400

    Reword call to action

--- src/components/CalltoAction.js
+++ src/components/CalltoAction.js
@@ -7,7 +7,7 @@ function CalltoAction() {
     <Container id="hireMe" className="pb-4 my-5" style={{ justifyContent: "center" }}>
       <Row style={{ flexDirection: "column", alignItems: "center" }}>
         <p className="text-center">
-          Whether it's designing an unique brand logo for your business,
+          Whether it's designing an unique logo brand for your business,
           providing feedback to better enhance your website and gain new
           customers or bringing your idea to life, it is my goal to make your
           business goals a reality. <br /> Interested in Connecting?
